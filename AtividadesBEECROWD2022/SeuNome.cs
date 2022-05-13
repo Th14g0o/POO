3bf0e1f94069fa3b1a2db8556c4cbddd768572de
Thiago Gonsalves <93939243+Th14g0o@users.noreply.github.com>
2022-05-13 20:58:51 +0000

    Atualizei o nome do kkkkk para SeuNome

---
+++ AtividadesBEECROWD2022/SeuNome.cs
@@ -0,0 +1,10 @@
+using System;
+class Program {
+  public static void Main() {
+
+    Console.WriteLine("Nome:");
+    string nome = Console.ReadLine();
+    Console.WriteLine(nome +"?, Nome fei ein...");
+    
+  } 
+}
