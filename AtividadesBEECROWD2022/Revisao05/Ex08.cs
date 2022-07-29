e8c9c0971d97845b5483804e3f4cba9d3cf23b88
Thiago Gonsalves <93939243+Th14g0o@users.noreply.github.com>
2022-07-29 14:10:58 +0000

    8

--- AtividadesBEECROWD2022/Revisao05/Ex08.cs
+++ AtividadesBEECROWD2022/Revisao05/Ex08.cs
@@ -9,9 +9,9 @@ class Programa{
   public static int MenorInteiro(double x){
     //Função teto
     string teste = x.ToString();
-    if (teste.IndexOf('.') != -1) return ((int)x);
-    else if (x<0) return ((int)x+1);
-    else return ((int)x) +1;  
+    if (teste.IndexOf('.') != -1 && x<0) return ((int)x);
+    else if (x < 0) return ((int)x+1);
+    else return ((int)x+1) ;  
     
   }
 }
